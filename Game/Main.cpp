6969ca0a9ea2910f6f074727cc3c33419bda7441
Adam Howard <adamhoward25995@gmail.com>
2021-03-20 15:01:22 +0000

    Added basic networking stuff to connect to server, send and receive data, need to move it somewhere else

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -1,6 +1,21 @@
+//Prevents winsock 1.1 from being included
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#define _WINSOCK_DEPRECATED_NO_WARNINGS
+#endif
+
+#pragma comment(lib, "ws2_32.lib")
+
 #include <SFML/Graphics.hpp>
 #include "Game.h"
 #include "GameBoard.h"
+#include <Windows.h>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
 
 #define GAME_WIDTH 900
 #define GAME_HEIGHT 800
@@ -8,6 +23,50 @@
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(GAME_WIDTH, GAME_HEIGHT), "Connect 4", sf::Style::Titlebar | sf::Style::Close);
+
+	//Winsock connection to server
+	WSADATA wsa_data;
+	if (WSAStartup(MAKEWORD(2, 2), &wsa_data) != 0)
+		return EXIT_FAILURE;
+
+	//Create a Socket
+	SOCKET s;
+
+	if ((s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP)) == INVALID_SOCKET)
+		return EXIT_FAILURE;
+
+	//Connect to Server
+	struct sockaddr_in server;
+	server.sin_addr.S_un.S_addr = inet_addr("127.0.0.1");
+	server.sin_family = AF_INET;
+	server.sin_port = htons(8888);
+
+	//Connect to remote server
+	if (connect(s, (struct sockaddr *) &server, sizeof(server)) < 0)
+	{
+		closesocket(s);
+		WSACleanup();
+		return EXIT_FAILURE;
+	}
+
+	//Sends data to server
+	const int recvbuflen = 512;
+	const char *sendbuf = "Test from SFML Connect 4 game";
+	char recvbuf[recvbuflen];
+
+	int result;
+
+	//Send an initial buffer
+	result = send(s, sendbuf, (int)strlen(sendbuf), 0);
+	if (result == SOCKET_ERROR)
+	{
+		closesocket(s);
+		WSACleanup();
+		return EXIT_FAILURE;
+	}
+
+	closesocket(s);
+	WSACleanup();
 
 	//FPS
 	window.setFramerateLimit(60);
